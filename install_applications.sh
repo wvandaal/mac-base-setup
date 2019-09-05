8297918587e1c34d60dd56767779cfae07c37573
Willem van Daalen <willem@wcvd.me>
2019-09-05 14:42:12 -0400

    Create install_applications.sh

---
+++ install_applications.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install xcode CLI tools
+xcode-select --install
+
+# install cli tools
+brew install zsh
+brew install antigen
+
+# install apps
+brew cask install iterm2
+brew cask install slack
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install intellij-idea-ce
+brew cask install spotify
+brew cask install sizeup
+
+# clear the homebrew cache of unused formulae
+brew cleanup -s
