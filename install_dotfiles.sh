675ceba76f41a15f789ba3a5b2f53125ccced206
Willem van Daalen <willem@wcvd.me>
2019-09-05 20:14:44 -0400

    Update install_dotfiles.sh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+# Go to ~ directory
 cd
 
-# Download .zshrc
-curl -O https://raw.githubusercontent.com/wvandaal/mac-base-setup/master/.zshrc
+# Download .zshrc if it doesn't exist
+if [ ! -f .zshrc ]; then
+  curl -O https://raw.githubusercontent.com/wvandaal/mac-base-setup/master/.zshrc
+else
+  echo ".zshrc already exists"
+fi
 
-# Download .vimrc
-curl -O https://raw.githubusercontent.com/wvandaal/mac-base-setup/master/.vimrc
+# Download .vimrc if it doesn't exist
+if [ ! -f .vimrc ]; then
+  curl -O https://raw.githubusercontent.com/wvandaal/mac-base-setup/master/.vimrc
+else
+  echp ".vimrc already exists"
+fi
